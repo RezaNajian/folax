9a13380cc76702778ff64db7d1c7aa66ef897886
RezaNajian <r.najian@hotmail.com>
2024-09-09 07:38:55 +0200

    mesh_io is updated and adjusted

--- fol/IO/mesh_io.py
+++ fol/IO/mesh_io.py
@@ -18,56 +18,35 @@ class MeshIO(InputOutput):
 
     """
     @print_with_timestamp_and_execution_time
-    def __init__(self,io_name:str,case_dir:str,file_name:str,bc_settings:dict) -> None:
+    def __init__(self,io_name:str,file_name:str,case_dir:str=".") -> None:
         super().__init__(io_name)
         self.file_name = file_name
         self.case_dir = case_dir
-        self.bc_settings = bc_settings
 
+    @print_with_timestamp_and_execution_time
     def Initialize(self) -> None:
-        pass
-
-    # @print_with_timestamp_and_execution_time
-    def Import(self) -> None:
         self.mesh_io = meshio.read(os.path.join(self.case_dir, self.file_name))
         self.mesh_io.point_data_to_sets('point_tags')
         self.mesh_io.cell_data_to_sets('cell_tags')
+        self.node_ids = jnp.arange(len(self.mesh_io.points))
+        self.nodes_coordinates = jnp.array(self.mesh_io.points)
+        
+        #create elemnt nodes dict based on element types
+        self.elements_nodes = {}
+        for elements_info in self.mesh_io.cells:
+            self.elements_nodes[elements_info.type] = jnp.array(elements_info.data)
+        
+        # create node sets
+        self.point_sets = {}
+        for tag,tag_info_list in self.mesh_io.point_tags.items():
+            if len(tag_info_list)>1:
+                point_set_name = tag_info_list[1]
+                self.point_sets[point_set_name] = jnp.array(self.mesh_io.point_sets[f"set-key-{tag}"])
+                
+        # TODO: create element sets 
+        self.element_sets = {}
 
-        self.dofs_dict = {}
-        for dof_name,dof_settings in self.bc_settings.items():
-            fol_dof_settings = {"non_dirichlet_nodes_ids":[],
-                                "dirichlet_nodes_ids":[],
-                                "dirichlet_nodes_dof_value":[]}
-            for boundary_name,boundary_value in dof_settings.items():
-                boundary_tag = None
-                for tag_value,tage_names in self.mesh_io.point_tags.items():
-                    if boundary_name in tage_names:
-                        boundary_tag = tag_value
-                if boundary_tag == None:
-                    raise ValueError(f"boundary {boundary_name} does not exist !")
-                else:
-                    boundary_node_ids = self.mesh_io.point_sets[f"set-key-{boundary_tag}"]
-                    boundary_node_values = [boundary_value] * len(boundary_node_ids)
-                    fol_dof_settings["dirichlet_nodes_ids"].extend(boundary_node_ids)
-                    fol_dof_settings["dirichlet_nodes_dof_value"].extend(boundary_node_values)
-
-            for point_index in range(len(self.mesh_io.points)):
-                if not point_index in fol_dof_settings["dirichlet_nodes_ids"]:
-                    fol_dof_settings["non_dirichlet_nodes_ids"].append(point_index)
-
-            fol_dof_settings["dirichlet_nodes_dof_value"] = np.array(fol_dof_settings["dirichlet_nodes_dof_value"])
-            fol_dof_settings["non_dirichlet_nodes_ids"] = np.array(fol_dof_settings["non_dirichlet_nodes_ids"])
-            fol_dof_settings["dirichlet_nodes_ids"] = np.array(fol_dof_settings["dirichlet_nodes_ids"])
-
-            self.dofs_dict[dof_name] = fol_dof_settings
-
-        self.nodes_dict = {"nodes_ids":jnp.arange(len(self.mesh_io.points)),"X":self.mesh_io.points[:,0],
-                           "Y":self.mesh_io.points[:,1],"Z":self.mesh_io.points[:,2]}
-        self.elements_dict = {"elements_ids":jnp.arange(len(self.mesh_io.cells_dict['tetra'])),
-                        "elements_nodes":jnp.array(self.mesh_io.cells_dict['tetra'])}
-
-    
-        return {"nodes_dict":self.nodes_dict,"elements_dict":self.elements_dict,"dofs_dict":self.dofs_dict}
+        self.is_initialized = True
 
     def Finalize(self) -> None:
         pass
