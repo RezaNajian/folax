eb227a9a05f311fe18bf7d93c91fa0aea2466236
RezaNajian <r.najian@hotmail.com>
2024-09-10 08:01:04 +0200

    unified mesh and mdpa io and updated the test

--- tests/integration/test_mesh_io.py
+++ tests/integration/test_mesh_io.py
@@ -2,10 +2,10 @@ import pytest
 import unittest
 import os
 import numpy as np
-from fol.IO.mdpa_io import MdpaIO
+from fol.mesh_input_output.mesh_io import MeshIO
 from fol.tools.usefull_functions import *
 
-class TestMdpaIO(unittest.TestCase):
+class TestMeshIO(unittest.TestCase):
 
     @pytest.fixture(autouse=True)
     def _request_debug_mode(self,request):
@@ -13,10 +13,12 @@ class TestMdpaIO(unittest.TestCase):
 
     def setUp(self):
         # problem setup
-        test_name = 'test_mdpa_io'
+        test_name = 'test_mesh_io'
         self.test_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), test_name)
         create_clean_directory(self.test_directory)
-        self.mdpa_io = MdpaIO("mdpa_io",file_name="coarse_sphere.mdpa",
+        self.mdpa_io = MeshIO("mdpa_io",file_name="coarse_sphere.mdpa",
+                              case_dir=os.path.join(os.path.dirname(os.path.abspath(__file__)),"meshes"))
+        self.med_io = MeshIO("med_io",file_name="box_3D_coarse.med",
                               case_dir=os.path.join(os.path.dirname(os.path.abspath(__file__)),"meshes"))
 
     def test_io(self):
@@ -25,6 +27,9 @@ class TestMdpaIO(unittest.TestCase):
         self.assertEqual(self.mdpa_io.GetNumberOfElements("tetra"),249)
         self.assertEqual(len(self.mdpa_io.GetNodeSet("Skin_Part")),66)
         self.assertEqual(len(self.mdpa_io.GetNodeSet("Partial_Skin_Part")),13)
+        self.med_io.Initialize()
+        self.assertEqual(self.med_io.GetNumberOfNodes(),1213)
+        self.assertEqual(self.med_io.GetNumberOfElements("tetra"),4973)
 
         try:
             field_to_export = np.zeros(self.mdpa_io.GetNumberOfNodes())
@@ -32,6 +37,10 @@ class TestMdpaIO(unittest.TestCase):
             field_to_export[self.mdpa_io.GetNodeSet("Partial_Skin_Part")] = 2*np.ones((len(self.mdpa_io.GetNodeSet("Partial_Skin_Part"))))
             self.mdpa_io['T'] = field_to_export
             self.mdpa_io.Finalize(export_dir=self.test_directory)
+
+            field_to_export = np.zeros(self.med_io.GetNumberOfNodes())
+            self.med_io['T'] = field_to_export
+            self.med_io.Finalize(export_dir=self.test_directory)
         except Exception as e:
             pytest.fail("value assignment & export failed")
 
