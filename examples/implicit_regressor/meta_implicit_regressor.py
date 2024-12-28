142e115125546a94fd33b66128e5222dc95ff74d
RezaNajian <r.najian@hotmail.com>
2024-12-28 12:02:16 +0100

    final updates and cleanings

--- examples/implicit_regressor/meta_implicit_regressor.py
+++ examples/implicit_regressor/meta_implicit_regressor.py
@@ -5,14 +5,14 @@ import numpy as np
 from fol.loss_functions.regression_loss import RegressionLoss
 from fol.mesh_input_output.mesh import Mesh
 from fol.controls.fourier_control import FourierControl
-from fol.deep_neural_networks.meta_implicit_auto_decoder_operator_learning import MetaImplicitAutodecoderOperatorLearning
+from fol.deep_neural_networks.meta_implicit_parametric_operator_learning import MetaImplicitParametricOperatorLearning
 from fol.tools.usefull_functions import *
 from fol.tools.logging_functions import Logger
 from fol.deep_neural_networks.nns import HyperNetwork,MLP
 import pickle
 
 # directory & save handling
-working_directory_name = 'meta_learning_implicit_regressor'
+working_directory_name = 'meta_implicit_regressor'
 case_dir = os.path.join('.', working_directory_name)
 create_clean_directory(working_directory_name)
 sys.stdout = Logger(os.path.join(case_dir,working_directory_name+".log"))
@@ -75,47 +75,48 @@ synthesizer_nn = MLP(name="regressor_synthesizer",
 
 latent_size = 10
 modulator_nn = MLP(name="modulator_nn",
-                   input_size=latent_size) 
+                   input_size=latent_size,
+                   use_bias=False) 
 
 hyper_network = HyperNetwork(name="hyper_nn",
                              modulator_nn=modulator_nn,synthesizer_nn=synthesizer_nn,
                              coupling_settings={"modulator_to_synthesizer_coupling_mode":"one_modulator_per_synthesizer_layer"})
 
 # create fol optax-based optimizer
-learning_rate_scheduler = optax.linear_schedule(init_value=1e-4, end_value=1e-6, transition_steps=10000)
-main_loop_transform = optax.chain(optax.adam(1e-5))
-latent_step_optimizer = optax.chain(optax.adam(1e-6))
+num_epochs = 5000
+learning_rate_scheduler = optax.linear_schedule(init_value=1e-4, end_value=1e-7, transition_steps=num_epochs)
+main_loop_transform = optax.chain(optax.normalize_by_update_norm(),optax.adam(learning_rate_scheduler))
 
 # create fol
-fol = MetaImplicitAutodecoderOperatorLearning(name="meta_implicit_ol",control=fourier_control,
+fol = MetaImplicitParametricOperatorLearning(name="meta_implicit_ol",control=fourier_control,
                                                 loss_function=reg_loss,
                                                 flax_neural_network=hyper_network,
                                                 main_loop_optax_optimizer=main_loop_transform,
-                                                latent_step_optax_optimizer=latent_step_optimizer,
                                                 latent_step_size=1e-2,
+                                                num_latent_iterations=3,
                                                 checkpoint_settings={"restore_state":False,
-                                                "state_directory":case_dir+"/flax_state",
-                                                "meta_state_directory":case_dir+"/meta_state"},
+                                                "state_directory":case_dir+"/flax_state"},
                                                 working_directory=case_dir)
 
 fol.Initialize()
 
 train_start_id = 0
-train_end_id = 10
+train_end_id = 20
+test_start_id = 3 * train_end_id
+test_end_id = 4 * train_end_id
 
 # here we train for single sample at eval_id but one can easily pass the whole coeffs_matrix
-fol.Train(train_set=(coeffs_matrix[train_start_id:train_end_id,:],),batch_size=1,
-          convergence_settings={"num_epochs":2000,"relative_error":1e-100,"absolute_error":1e-100},
-          plot_settings={"plot_save_rate":100},
-          save_settings={"save_nn_model":True,
+fol.Train(train_set=(coeffs_matrix[train_start_id:train_end_id,:],),
+          test_set=(coeffs_matrix[test_start_id:test_end_id,:],),
+           test_settings={"test_frequency":10},batch_size=1,
+            convergence_settings={"num_epochs":num_epochs,"relative_error":1e-100,"absolute_error":1e-100},
+            plot_settings={"plot_save_rate":100},
+            save_settings={"save_nn_model":True,
                          "best_model_checkpointing":True,
                          "best_model_checkpointing_frequency":10})
-
-
 # load teh best model
 fol.RestoreCheckPoint(fol.checkpoint_settings)
-
-for eval_id in list(np.arange(train_start_id,train_end_id)):
+for eval_id in list(np.arange(train_start_id,test_end_id)):
     fe_mesh[f'Pred_K_{eval_id}'] = np.array(fol.Predict(coeffs_matrix[eval_id,:].reshape(-1,1).T)).reshape(-1)
     fe_mesh[f'GT_K_{eval_id}'] = np.array(K_matrix[eval_id,:])
     fe_mesh[f'abs_error_{eval_id}'] = abs(fe_mesh[f'Pred_K_{eval_id}']-fe_mesh[f'GT_K_{eval_id}'])
--- fol/deep_neural_networks/meta_implicit_parametric_operator_learning.py
+++ fol/deep_neural_networks/meta_implicit_parametric_operator_learning.py
@@ -1,19 +1,15 @@
 """
  Authors: Reza Najian Asl, https://github.com/RezaNajian
- Date: November, 2024
+ Date: December, 2024
  License: FOL/LICENSE
 """
 
 from typing import Tuple 
 import jax
 import jax.numpy as jnp
-import copy
-import optax
 from functools import partial
 from optax import GradientTransformation
 from flax import nnx
-import orbax.checkpoint as orbax
-import orbax.checkpoint as ocp
 from .implicit_parametric_operator_learning import ImplicitParametricOperatorLearning
 from fol.tools.decoration_functions import *
 from fol.loss_functions.loss import Loss
@@ -21,7 +17,7 @@ from fol.controls.control import Control
 from fol.tools.usefull_functions import *
 from .nns import HyperNetwork
 
-class MetaImplicitAutodecoderOperatorLearning(ImplicitParametricOperatorLearning):
+class MetaImplicitParametricOperatorLearning(ImplicitParametricOperatorLearning):
     """
     A class for meta-learning implicit parametric operators using deep neural networks.
 
@@ -47,8 +43,8 @@ class MetaImplicitAutodecoderOperatorLearning(ImplicitParametricOperatorLearning
                  loss_function:Loss,
                  flax_neural_network:HyperNetwork,
                  main_loop_optax_optimizer:GradientTransformation,
-                 latent_step_optax_optimizer:GradientTransformation,
-                 latent_step_size:float,
+                 latent_step_size:float=1e-2,
+                 num_latent_iterations:int=3,
                  checkpoint_settings:dict={},
                  working_directory='.'
                  ):
@@ -69,15 +65,11 @@ class MetaImplicitAutodecoderOperatorLearning(ImplicitParametricOperatorLearning
                          main_loop_optax_optimizer,checkpoint_settings,
                          working_directory)
         
-        self.latent_step_optimizer = latent_step_optax_optimizer
         self.latent_step = latent_step_size
-        self.latent_step_optimizer_state = self.latent_step_optimizer.init(self.latent_step)
-        self.default_checkpoint_settings = {"restore_state":False,
-                                            "state_directory":'./flax_state',
-                                            "meta_state_directory":'./meta_state'}
-
+        self.num_latent_iterations = num_latent_iterations
+        
     @partial(nnx.jit, static_argnums=(0,))
-    def ComputeSingleLossValue(self,orig_features:Tuple[jnp.ndarray, jnp.ndarray],nn_model:nnx.Module,latent_opt_step:float):
+    def ComputeSingleLossValue(self,orig_features:Tuple[jnp.ndarray, jnp.ndarray],nn_model:nnx.Module):
         """
         Computes the single loss value for a given input using the latent code and the neural network model.
 
@@ -103,112 +95,12 @@ class MetaImplicitAutodecoderOperatorLearning(ImplicitParametricOperatorLearning
             return self.loss_function.ComputeSingleLoss(control_output,nn_output)[0]
 
         loss_latent_grad_fn = jax.grad(loss)
-        for _ in range(3):
-            latent_code -= latent_opt_step * loss_latent_grad_fn(latent_code)
+        for _ in range(self.num_latent_iterations):
+            grads = loss_latent_grad_fn(latent_code)
+            latent_code -= self.latent_step * grads / jnp.linalg.norm(grads)
         
         nn_output = nn_model(latent_code,self.loss_function.fe_mesh.GetNodesCoordinates()).flatten()[self.loss_function.non_dirichlet_indices]
         return self.loss_function.ComputeSingleLoss(control_output,nn_output)
-
-    @partial(nnx.jit, static_argnums=(0,))
-    def ComputeBatchLossValue(self,batch_set:Tuple[jnp.ndarray, jnp.ndarray],nn_model:nnx.Module,latent_opt_step:float):
-        """
-        Computes the loss values for a batch of data.
-
-        This method computes the network's output for a batch of input data, applies the control parameters,
-        and evaluates the loss function for the entire batch. It aggregates the results and returns
-        summary statistics (min, max, avg) for the batch losses.
-
-        Parameters
-        ----------
-        batch_set : Tuple[jnp.ndarray, jnp.ndarray]
-            A tuple containing a batch of input data and corresponding target labels.
-        nn_model : nnx.Module
-            The Flax neural network model.
-
-        Returns
-        -------
-        Tuple[jnp.ndarray, dict]
-            The mean loss for the batch and a dictionary of loss statistics (min, max, avg, total).
-        """
-
-        batch_losses,(batch_mins,batch_maxs,batch_avgs) = jax.vmap(self.ComputeSingleLossValue,(0,None,None))(batch_set,nn_model,latent_opt_step)
-        loss_name = self.loss_function.GetName()
-        total_mean_loss = jnp.mean(batch_losses)
-        return total_mean_loss, ({loss_name+"_min":jnp.min(batch_mins),
-                                         loss_name+"_max":jnp.max(batch_maxs),
-                                         loss_name+"_avg":jnp.mean(batch_avgs),
-                                         "total_loss":total_mean_loss})
-
-    @partial(jax.jit, static_argnums=(0,))
-    def TrainMetaStep(self, nnx_graphdef:nnx.GraphDef, nxx_state:nnx.GraphState, 
-                      latent_opt_state:optax.OptState,latent_step:float,
-                      train_batch:Tuple[jnp.ndarray, jnp.ndarray]):
-
-        nnx_model, nnx_optimizer = nnx.merge(nnx_graphdef, nxx_state)
-
-        (batch_loss, batch_dict), batch_grads = nnx.value_and_grad(self.ComputeBatchLossValue,argnums=(1,2),has_aux=True) \
-                                                                    (train_batch,nnx_model,self.latent_step)
-        
-        latent_step_update, new_latent_opt_state = self.latent_step_optimizer.update(batch_grads[1], latent_opt_state)
-        updated_latent_step = optax.apply_updates(latent_step, latent_step_update)
-
-        nnx_optimizer.update(batch_grads[0])
-        _, new_state = nnx.split((nnx_model, nnx_optimizer))
-        return batch_dict,new_state,updated_latent_step,new_latent_opt_state
-    
-    def TrainStep(self, nnx_graphdef:nnx.GraphDef, nxx_state:nnx.GraphState, 
-                        train_batch:Tuple[jnp.ndarray, jnp.ndarray]):
-
-        batch_dict,new_state,self.latent_step,self.latent_step_optimizer_state = self.TrainMetaStep(nnx_graphdef,
-                                                                                           nxx_state,
-                                                                                           self.latent_step_optimizer_state,
-                                                                                           self.latent_step,
-                                                                                           train_batch)
-        return batch_dict,new_state
-
-    # def TestStep(self, nnx_graphdef:nnx.GraphDef, nxx_state:nnx.GraphState,test_batch:Tuple[jnp.ndarray, jnp.ndarray]):
-    #     """
-    #     Evaluates the model on a test batch.
-
-    #     This function computes the latent codes for a given test batch using `ComputeSingleLatentCode`. 
-    #     It then evaluates the batch loss using the computed latent codes and returns the loss statistics.
-
-    #     Args:
-    #         nnx_graphdef (nnx.GraphDef): The graph definition of the neural network model.
-    #         nxx_state (nnx.GraphState): The current state of the neural network and optimizer.
-    #         test_batch (Tuple[jnp.ndarray, jnp.ndarray]): A tuple containing:
-    #             - The original input features of the test batch.
-    #             - The corresponding targets or labels.
-
-    #     Returns:
-    #         Tuple[dict, nnx.GraphState]: A tuple containing:
-    #             - `test_batch_dict` (dict): A dictionary with batch loss statistics, such as minimum, maximum, and average loss values.
-    #             - `state` (nnx.GraphState): The updated state of the neural network and optimizer after the evaluation step.
-    #     """        
-    #     nnx_model,nnx_optimizer = nnx.merge(nnx_graphdef, nxx_state)
-
-    #     latent_codes = jax.vmap(self.ComputeSingleLatentCode,(0,None,None))(test_batch,   
-    #                                                                         nnx_model,
-    #                                                                         self.latent_optimizer)
-    #     (test_loss, test_batch_dict) = self.ComputeBatchLossValue(test_batch,latent_codes,nnx_model)
-    #     _, state = nnx.split((nnx_model, nnx_optimizer))
-    #     return test_batch_dict,state
-
-    def RestoreCheckPoint(self,checkpoint_settings:dict):
-        super().RestoreCheckPoint(checkpoint_settings)
-        if "meta_state_directory" in checkpoint_settings.keys():
-            meta_state_directory = checkpoint_settings["meta_state_directory"]
-            absolute_path = os.path.abspath(meta_state_directory)
-            self.latent_step = self.checkpointer.restore(absolute_path,{"latent_step":self.latent_step})["latent_step"]
-            fol_info(f"latent_step {self.latent_step} is restored from {meta_state_directory}")
-
-    def SaveCheckPoint(self):
-        super().SaveCheckPoint()
-        # save meta learning latent_step
-        state_directory = self.checkpoint_settings["meta_state_directory"]
-        absolute_path = os.path.abspath(state_directory)
-        self.checkpointer.save(absolute_path, {"latent_step":self.latent_step},force=True)
-        fol_info(f"latent_step {self.latent_step} is saved to {state_directory}")
 
     @print_with_timestamp_and_execution_time
     def Predict(self,batch_X:jnp.ndarray):
@@ -241,8 +133,9 @@ class MetaImplicitAutodecoderOperatorLearning(ImplicitParametricOperatorLearning
                 return self.loss_function.ComputeSingleLoss(control_output,nn_output)[0]
 
             loss_latent_grad_fn = jax.grad(loss)
-            for _ in range(3):
-                latent_code -= self.latent_step * loss_latent_grad_fn(latent_code)
+            for _ in range(self.num_latent_iterations):
+                grads = loss_latent_grad_fn(latent_code)
+                latent_code -= self.latent_step * grads / jnp.linalg.norm(grads)
 
             nn_output = self.flax_neural_network(latent_code,self.loss_function.fe_mesh.GetNodesCoordinates()).flatten()[self.loss_function.non_dirichlet_indices]
 
