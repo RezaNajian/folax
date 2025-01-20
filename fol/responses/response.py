4792a8e496a569f80f64bb54811358e7f7f4ada8
RezaNajian <r.najian@hotmail.com>
2025-01-20 19:16:34 +0100

    SA classes

---
+++ fol/responses/response.py
@@ -0,0 +1,37 @@
+"""
+ Authors: Reza Najian Asl, https://github.com/RezaNajian
+ Date: January, 2025
+ License: FOL/LICENSE
+"""
+from abc import ABC, abstractmethod
+
+class Response(ABC):
+
+    def __init__(self, response_name: str) -> None:
+        self.__name = response_name
+        self.initialized = False
+
+    def GetName(self) -> str:
+        return self.__name
+
+    @abstractmethod
+    def Initialize(self) -> None:
+        pass
+
+    @abstractmethod
+    def ComputeValue(self):
+        pass
+
+    @abstractmethod
+    def ComputeAdjointBasedGradients(self):
+        pass
+
+    def ComputeFDBasedGradients(self):
+        pass
+
+    @abstractmethod
+    def Finalize(self) -> None:
+        pass
+
+
+
