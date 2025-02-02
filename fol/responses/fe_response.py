95b4772990425b116b4a4fddb42bd241144ad5ac
RezaNajian <r.najian@hotmail.com>
2025-02-02 19:36:02 +0100

    adj sa updated and cleaned

--- fol/responses/fe_response.py
+++ fol/responses/fe_response.py
@@ -32,20 +32,9 @@ class FiniteElementResponse(Response):
         self.fe_loss.Initialize()
         self.control.Initialize()
 
-        # Dynamically generate symbolic variables
-        self.symbols_dict = {name: symbols(name) for name in self.fe_loss.dofs}
-        self.symbols_list = list(self.symbols_dict.values())  # Preserve order
-
-        # Define the vector U symbolically
-        U = Matrix(self.symbols_list)
-
-        # Parse the response formula using the generated symbols
-        formula_expr = parse_expr(self.response_formula, local_dict={"U": U, "Matrix": Matrix})
-
-        # Convert the expression to a JAX-compatible function
-        self.jit_function = jax.jit(
-            lambda U: lambdify(self.symbols_list, formula_expr, 'jax')(*U)
-        )
+        variables_list=[self.control.GetName(),self.fe_loss.dofs[0][0]]
+        func_str = f"lambda {', '.join(variables_list)}: {self.response_formula}"
+        self.jit_response_function = jax.jit(eval(func_str, {"jnp": jnp}))
 
         if self.fe_loss.dim == 2:
             self.CalculateNMatrix = self.CalculateNMatrix2D    
@@ -71,48 +60,49 @@ class FiniteElementResponse(Response):
         return N_mat        
 
     @partial(jit, static_argnums=(0,))
-    def ComputeElementValue(self,xyze,de,uvwe):
+    def ComputeResponseElementValue(self,xyze,de,uvwe):
         @jit
         def compute_at_gauss_point(gp_point,gp_weight):
             N_vec = self.fe_loss.fe_element.ShapeFunctionsValues(gp_point)
             N_mat = self.CalculateNMatrix(N_vec)
-            gp_dofs = N_mat @ uvwe
+            gp_dofs = (N_mat @ uvwe).flatten()
+            gp_d = jnp.dot(N_vec, de.squeeze())
             J = self.fe_loss.fe_element.Jacobian(xyze,gp_point)
             detJ = jnp.linalg.det(J)            
-            return gp_weight * detJ * self.jit_function(gp_dofs)
+            return gp_weight * detJ * self.jit_response_function(gp_d,gp_dofs)
 
         gp_points,gp_weights = self.fe_loss.fe_element.GetIntegrationData()
         v_gps = jax.vmap(compute_at_gauss_point,in_axes=(0,0))(gp_points,gp_weights)
         return  jnp.sum(v_gps)
     
     @partial(jit, static_argnums=(0,))
-    def ComputeElementValueStateGrad(self,xyze,de,uvwe):
-        return jax.grad(self.ComputeElementValue,argnums=2)(xyze,de,uvwe)
+    def ComputeResponseElementValueStateGrad(self,xyze,de,uvwe):
+        return jax.grad(self.ComputeResponseElementValue,argnums=2)(xyze,de,uvwe)
     
     @partial(jit, static_argnums=(0,))
-    def ComputeElementValueControlGrad(self,xyze,de,uvwe):
-        return jax.grad(self.ComputeElementValue,argnums=1)(xyze,de,uvwe)
+    def ComputeResponseElementValueControlGrad(self,xyze,de,uvwe):
+        return jax.grad(self.ComputeResponseElementValue,argnums=1)(xyze,de,uvwe)
     
     @partial(jit, static_argnums=(0,))
-    def ComputeElementValueShapeGrad(self,xyze,de,uvwe):
-        return jax.grad(self.ComputeElementValue,argnums=0)(xyze,de,uvwe)
+    def ComputeResponseElementValueShapeGrad(self,xyze,de,uvwe):
+        return jax.grad(self.ComputeResponseElementValue,argnums=0)(xyze,de,uvwe).flatten()
 
     @partial(jit, static_argnums=(0,))
-    def ComputeElementEnergyVmapCompatible(self,
+    def ComputeResponseElementValueVmapCompatible(self,
                                            element_id:jnp.integer,
                                            elements_nodes:jnp.array,
                                            xyz:jnp.array,
                                            full_control_vector:jnp.array,
                                            full_dof_vector:jnp.array):
-        return self.ComputeElementValue(xyz[elements_nodes[element_id],:],
+        return self.ComputeResponseElementValue(xyz[elements_nodes[element_id],:],
                                          full_control_vector[elements_nodes[element_id]],
                                          full_dof_vector[((self.fe_loss.number_dofs_per_node*elements_nodes[element_id])[:, jnp.newaxis] +
                                          jnp.arange(self.fe_loss.number_dofs_per_node))].reshape(-1,1))
 
     @partial(jit, static_argnums=(0,))
-    def ComputeElementsValues(self,total_control_vars:jnp.array,total_primal_vars:jnp.array):
+    def ComputeResponseElementsValues(self,total_control_vars:jnp.array,total_primal_vars:jnp.array):
         # parallel calculation of element values
-        return jax.vmap(self.ComputeElementEnergyVmapCompatible,(0,None,None,None,None)) \
+        return jax.vmap(self.ComputeResponseElementValueVmapCompatible,(0,None,None,None,None)) \
                         (self.fe_loss.fe_mesh.GetElementsIds(self.fe_loss.element_type),
                         self.fe_loss.fe_mesh.GetElementsNodes(self.fe_loss.element_type),
                         self.fe_loss.fe_mesh.GetNodesCoordinates(),
@@ -122,14 +112,14 @@ class FiniteElementResponse(Response):
     @print_with_timestamp_and_execution_time
     @partial(jit, static_argnums=(0,))
     def ComputeValue(self,nodal_control_values:jnp.array,nodal_dof_values:jnp.array):
-        return jnp.sum(self.ComputeElementsValues(nodal_control_values,nodal_dof_values))
+        return jnp.sum(self.ComputeResponseElementsValues(nodal_control_values,nodal_dof_values))
 
     @partial(jit, static_argnums=(0,))
     def ComputeElementRHS(self,
                           elem_xyz:jnp.array,
                           elem_controls:jnp.array,
                           elem_dofs:jnp.array):
-        return self.ComputeElementValueStateGrad(elem_xyz,elem_controls,elem_dofs)
+        return self.ComputeResponseElementValueStateGrad(elem_xyz,elem_controls,elem_dofs)
 
     @partial(jit, static_argnums=(0,))
     def ComputeElementRHSVmapCompatible(self,element_id:jnp.integer,
@@ -170,57 +160,123 @@ class FiniteElementResponse(Response):
         return sparse_jacobian,rhs_vector
 
     @partial(jit, static_argnums=(0,))
-    def ComputeResponseNodalControlDerivativeVmapCompatible(self,element_id:jnp.integer,
+    def ComputeResponseLocalNodalShapeDerivativesVmapCompatible(self,element_id:jnp.integer,
                                             elements_nodes:jnp.array,
                                             xyz:jnp.array,
                                             full_control_vector:jnp.array,
                                             full_dof_vector:jnp.array):
-        return self.ComputeElementValueControlGrad(xyz[elements_nodes[element_id],:],
+        return self.ComputeResponseElementValueShapeGrad(xyz[elements_nodes[element_id],:],
                                                     full_control_vector[elements_nodes[element_id]],
                                                     full_dof_vector[((self.fe_loss.number_dofs_per_node*elements_nodes[element_id])[:, jnp.newaxis] +
                                                                     jnp.arange(self.fe_loss.number_dofs_per_node))].reshape(-1,1))
 
-    def ComputeResponseNodalControlDerivatives(self,nodal_control_values:jnp.array,nodal_dof_values:jnp.array):
-        elements_control_derv = jax.vmap(self.ComputeResponseNodalControlDerivativeVmapCompatible,(0,None,None,None,None)) \
+    @partial(jit, static_argnums=(0,))
+    def ComputeLossElementShapeGrad(self,xyze,de,uvwe,adj_uvwe):
+        jacobian_fn = jax.jacrev(lambda *args: self.fe_loss.ComputeElement(*args)[1], argnums=0)
+        res_shape_grads = jnp.squeeze(jacobian_fn(xyze, de, uvwe))
+        res_shape_grads = res_shape_grads.reshape(*res_shape_grads.shape[:-2], -1)
+        return (adj_uvwe.T @ res_shape_grads).flatten()
+
+    @partial(jit, static_argnums=(0,))
+    def ComputeAdjointLossElementShapeDerivativesVmapCompatible(self,element_id:jnp.integer,
+                                            elements_nodes:jnp.array,
+                                            xyz:jnp.array,
+                                            full_control_vector:jnp.array,
+                                            full_dof_vector:jnp.array,
+                                            full_adj_dof_vector:jnp.array):
+        return self.ComputeLossElementShapeGrad(xyz[elements_nodes[element_id],:],
+                                                    full_control_vector[elements_nodes[element_id]],
+                                                    full_dof_vector[((self.fe_loss.number_dofs_per_node*elements_nodes[element_id])[:, jnp.newaxis] +
+                                                                    jnp.arange(self.fe_loss.number_dofs_per_node))].reshape(-1,1),
+                                                    full_adj_dof_vector[((self.fe_loss.number_dofs_per_node*elements_nodes[element_id])[:, jnp.newaxis] +
+                                                                    jnp.arange(self.fe_loss.number_dofs_per_node))].reshape(-1,1)                                                                    
+                                                                    )
+
+    @print_with_timestamp_and_execution_time
+    def ComputeAdjointNodalShapeDerivatives(self,nodal_control_values:jnp.array,
+                                                 nodal_dof_values:jnp.array,
+                                                 nodal_adj_dof_values:jnp.array):
+        response_elements_local_shape_derv = jax.vmap(self.ComputeResponseLocalNodalShapeDerivativesVmapCompatible,(0,None,None,None,None)) \
                                                             (self.fe_loss.fe_mesh.GetElementsIds(self.fe_loss.element_type),
                                                              self.fe_loss.fe_mesh.GetElementsNodes(self.fe_loss.element_type),
                                                              self.fe_loss.fe_mesh.GetNodesCoordinates(),
                                                              nodal_control_values,
                                                              nodal_dof_values)
-        
+
+        elements_residuals_adj_shape_derv = jax.vmap(self.ComputeAdjointLossElementShapeDerivativesVmapCompatible,(0,None,None,None,None,None)) \
+                                                            (self.fe_loss.fe_mesh.GetElementsIds(self.fe_loss.element_type),
+                                                             self.fe_loss.fe_mesh.GetElementsNodes(self.fe_loss.element_type),
+                                                             self.fe_loss.fe_mesh.GetNodesCoordinates(),
+                                                             nodal_control_values,
+                                                             nodal_dof_values,
+                                                             nodal_adj_dof_values)
+
+        total_elem_shape_grads = response_elements_local_shape_derv + elements_residuals_adj_shape_derv
         # compute the global derivative vector
-        grad_vector = jnp.zeros((self.control.num_controlled_vars))
-        number_controls_per_node = int(self.control.num_controlled_vars / self.fe_loss.fe_mesh.GetNumberOfNodes())
+        grad_vector = jnp.zeros((3*self.fe_loss.fe_mesh.GetNumberOfNodes()))
+        number_controls_per_node = 3
         for control_idx in range(number_controls_per_node):
-            grad_vector = grad_vector.at[number_controls_per_node*self.fe_loss.fe_mesh.GetElementsNodes(self.fe_loss.element_type)+control_idx].add(jnp.squeeze(elements_control_derv[:,control_idx::number_controls_per_node]))
+            grad_vector = grad_vector.at[number_controls_per_node*self.fe_loss.fe_mesh.GetElementsNodes(self.fe_loss.element_type)+control_idx].add(jnp.squeeze(total_elem_shape_grads[:,control_idx::number_controls_per_node]))
 
         return grad_vector
-    
+
     @partial(jit, static_argnums=(0,))
-    def ComputeResponseShapeDerivativeVmapCompatible(self,element_id:jnp.integer,
+    def ComputeResponseLocalNodalControlDerivativesVmapCompatible(self,element_id:jnp.integer,
                                             elements_nodes:jnp.array,
                                             xyz:jnp.array,
                                             full_control_vector:jnp.array,
                                             full_dof_vector:jnp.array):
-        return self.ComputeElementValueShapeGrad(xyz[elements_nodes[element_id],:],
+        return self.ComputeResponseElementValueControlGrad(xyz[elements_nodes[element_id],:],
                                                     full_control_vector[elements_nodes[element_id]],
                                                     full_dof_vector[((self.fe_loss.number_dofs_per_node*elements_nodes[element_id])[:, jnp.newaxis] +
                                                                     jnp.arange(self.fe_loss.number_dofs_per_node))].reshape(-1,1))
 
-    def ComputeResponseShapeDerivatives(self,nodal_control_values:jnp.array,nodal_dof_values:jnp.array):
-        elements_shape_derv = jax.vmap(self.ComputeResponseShapeDerivativeVmapCompatible,(0,None,None,None,None)) \
+    @partial(jit, static_argnums=(0,))
+    def ComputeLossElementControlGrad(self,xyze,de,uvwe,adj_uvwe):
+        jacobian_fn = jax.jacrev(lambda *args: self.fe_loss.ComputeElement(*args)[1], argnums=1)
+        res_control_grads = jnp.squeeze(jacobian_fn(xyze, de, uvwe))
+        return (adj_uvwe.T @ res_control_grads).flatten()
+
+    @partial(jit, static_argnums=(0,))
+    def ComputeAdjointLossElementControlDerivativesVmapCompatible(self,element_id:jnp.integer,
+                                            elements_nodes:jnp.array,
+                                            xyz:jnp.array,
+                                            full_control_vector:jnp.array,
+                                            full_dof_vector:jnp.array,
+                                            full_adj_dof_vector:jnp.array):
+        return self.ComputeLossElementControlGrad(xyz[elements_nodes[element_id],:],
+                                                    full_control_vector[elements_nodes[element_id]],
+                                                    full_dof_vector[((self.fe_loss.number_dofs_per_node*elements_nodes[element_id])[:, jnp.newaxis] +
+                                                                    jnp.arange(self.fe_loss.number_dofs_per_node))].reshape(-1,1),
+                                                    full_adj_dof_vector[((self.fe_loss.number_dofs_per_node*elements_nodes[element_id])[:, jnp.newaxis] +
+                                                                    jnp.arange(self.fe_loss.number_dofs_per_node))].reshape(-1,1)                                                                    
+                                                                    )
+
+    @print_with_timestamp_and_execution_time
+    def ComputeAdjointNodalControlDerivatives(self,nodal_control_values:jnp.array,
+                                                   nodal_dof_values:jnp.array,
+                                                   nodal_adj_dof_values:jnp.array):
+        response_elements_local_control_derv = jax.vmap(self.ComputeResponseLocalNodalControlDerivativesVmapCompatible,(0,None,None,None,None)) \
                                                             (self.fe_loss.fe_mesh.GetElementsIds(self.fe_loss.element_type),
                                                              self.fe_loss.fe_mesh.GetElementsNodes(self.fe_loss.element_type),
                                                              self.fe_loss.fe_mesh.GetNodesCoordinates(),
                                                              nodal_control_values,
                                                              nodal_dof_values)
+
+        elements_residuals_adj_control_derv = jax.vmap(self.ComputeAdjointLossElementControlDerivativesVmapCompatible,(0,None,None,None,None,None)) \
+                                                            (self.fe_loss.fe_mesh.GetElementsIds(self.fe_loss.element_type),
+                                                             self.fe_loss.fe_mesh.GetElementsNodes(self.fe_loss.element_type),
+                                                             self.fe_loss.fe_mesh.GetNodesCoordinates(),
+                                                             nodal_control_values,
+                                                             nodal_dof_values,
+                                                             nodal_adj_dof_values)
         
-        llllll # not implemeneted ATTENTION!
+        total_elem_control_grads = response_elements_local_control_derv + elements_residuals_adj_control_derv
         # compute the global derivative vector
         grad_vector = jnp.zeros((self.control.num_controlled_vars))
-        number_controls_per_node = int(self.control.num_controlled_vars / self.fe_loss.fe_mesh.GetNumberOfNodes())
+        number_controls_per_node = number_controls_per_node = int(self.control.num_controlled_vars / self.fe_loss.fe_mesh.GetNumberOfNodes())
         for control_idx in range(number_controls_per_node):
-            grad_vector = grad_vector.at[number_controls_per_node*self.fe_loss.fe_mesh.GetElementsNodes(self.fe_loss.element_type)+control_idx].add(jnp.squeeze(elements_control_derv[:,control_idx::number_controls_per_node]))
+            grad_vector = grad_vector.at[number_controls_per_node*self.fe_loss.fe_mesh.GetElementsNodes(self.fe_loss.element_type)+control_idx].add(jnp.squeeze(total_elem_control_grads[:,control_idx::number_controls_per_node]))
 
         return grad_vector
 
