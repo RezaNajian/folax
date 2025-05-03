5488124e212925ff704ef470f12288d1f4e5d054
RezaNajian <r.najian@hotmail.com>
2025-05-03 06:36:38 +0200

    transient thermal loss and its example completed

--- examples/paper_iFOL/transient_nonlinear_thermal_2D/meta_implicit_pl_transient_nonlinear_thermal_2D.py
+++ examples/paper_iFOL/transient_nonlinear_thermal_2D/meta_implicit_pl_transient_nonlinear_thermal_2D.py
@@ -1,21 +1,21 @@
 import sys
 import os
-import optax
+import pickle,optax
 import numpy as np
-
 from fol.tools.usefull_functions import *
+from fol.tools.decoration_functions import *
 from fol.tools.logging_functions import Logger
 from fol.controls.identity_control import IdentityControl
 from fol.deep_neural_networks.meta_alpha_meta_implicit_parametric_operator_learning import MetaAlphaMetaImplicitParametricOperatorLearning
+from fol.solvers.fe_nonlinear_residual_based_solver import FiniteElementNonLinearResidualBasedSolver
 from fol.deep_neural_networks.nns import HyperNetwork,MLP
 from fol.loss_functions.transient_thermal import TransientThermalLoss2DQuad
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+from thermal_usefull_functions import *
 
 # directory & save handling
 working_directory_name = 'meta_implicit_pl_transient_nonlinear_thermal_2D'
 case_dir = os.path.join('.', working_directory_name)
-create_clean_directory(working_directory_name)
+# create_clean_directory(working_directory_name)
 sys.stdout = Logger(os.path.join(case_dir,working_directory_name+".log"))
 
 # problem setup
@@ -26,11 +26,28 @@ model_settings = {"L":1,"N":51,
 fe_mesh = create_2D_square_mesh(L=model_settings["L"],N=model_settings["N"])
 fe_mesh.Initialize()
 
+# create some random fields for training
+create_random_fields = False
+if create_random_fields:
+    train_temperature_fields = generate_random_smooth_patterns(model_settings["L"],model_settings["N"],num_samples=200)
+    train_heterogeneity_field = generate_morph_pattern(model_settings["N"]).reshape(1,-1) 
+    train_data_dict = {"temperatures":train_temperature_fields,"heterogeneity":train_heterogeneity_field}
+    with open(f'train_data_dict.pkl', 'wb') as f:
+        pickle.dump(train_data_dict,f)
+else:
+    with open(f'train_data_dict.pkl', 'rb') as f:
+        train_data_dict = pickle.load(f)
+    
+    train_temperature_fields = train_data_dict["temperatures"]
+    train_heterogeneity_field = train_data_dict["heterogeneity"]
+
+    fol_info(f"train temperature field {train_temperature_fields.shape} is imported !")
+    fol_info(f"train heterogeneity field {train_heterogeneity_field.shape} is imported !")
+    
+
 # create fe-based loss function
 bc_dict = {"T":{"left":model_settings["T_left"],"right":model_settings["T_right"]}}
-
-k0 = np.load("k0.npy").flatten()
-material_dict = {"rho":1.0,"cp":1.0,"k0":k0,"beta":1.0,"c":4.0}
+material_dict = {"rho":1.0,"cp":1.0,"k0":train_heterogeneity_field.flatten(),"beta":1.5,"c":1.0}
 time_integration_dict = {"method":"implicit-euler","time_step":0.005}
 transient_thermal_loss_2d = TransientThermalLoss2DQuad("thermal_transient_loss_2d",loss_settings={"dirichlet_bc_dict":bc_dict,
                                                                             "num_gp":2,
@@ -63,11 +80,11 @@ hyper_network = HyperNetwork(name="hyper_nn",
                              coupling_settings={"modulator_to_synthesizer_coupling_mode":"one_modulator_per_synthesizer_layer"})
 
 # create fol optax-based optimizer
-num_epochs = 2000
+num_epochs = 200
 learning_rate_scheduler = optax.linear_schedule(init_value=1e-4, end_value=1e-7, transition_steps=num_epochs)
 main_loop_transform = optax.chain(optax.adam(1e-5))
 latent_step_optimizer = optax.chain(optax.adam(1e-4))
-# create fol
+# create ifol
 ifol = MetaAlphaMetaImplicitParametricOperatorLearning(name="meta_implicit_ol",
                                                         control=identity_control,
                                                         loss_function=transient_thermal_loss_2d,
@@ -79,83 +96,83 @@ ifol = MetaAlphaMetaImplicitParametricOperatorLearning(name="meta_implicit_ol",
 ifol.Initialize()
 
 
-# train_start_id = 0
-# train_end_id = 6000
+train_start_id = 0
+train_end_id = 10
 
-# fol.Train(train_set=(coeffs_matrix[train_start_id:train_end_id,:],),
-#             batch_size=120,
-#             convergence_settings={"num_epochs":num_epochs,"relative_error":1e-100,"absolute_error":1e-100},
-#             plot_settings={"plot_save_rate":100})
+ifol.Train(train_set=(train_temperature_fields[train_start_id:train_end_id,:],),
+            batch_size=1,
+            convergence_settings={"num_epochs":num_epochs,"relative_error":1e-100,"absolute_error":1e-100},
+            plot_settings={"plot_save_rate":100},
+            working_directory=case_dir)
 
+ifol.RestoreState(restore_state_directory=case_dir+"/flax_final_state")
 
-exit()
+# time ineference setup
+initial_solution_id = 0 
+initial_solution = train_temperature_fields[initial_solution_id]
+num_time_steps = 50
 
-fol.RestoreState(os.path.join(".","flax_final_state"))
-num_steps = 50
+# predict dynamics with ifol
+T_ifols = ifol.PredictDynamics(initial_solution,num_time_steps)
 
-# FOL inference
-eval_id = 2
-FOL_T = np.array(fol.Predict_all(coeffs_matrix[eval_id,:].reshape(1,-1),num_steps))  
-FOL_T = np.squeeze(FOL_T,axis=1).T    
-fe_mesh['T_FOL'] = FOL_T    
-# solve FE here
-fe_setting = {"linear_solver_settings":{"solver":"JAX-bicgstab","tol":1e-6,"atol":1e-6,
-                                            "maxiter":1000,"pre-conditioner":"ilu"},
-                "nonlinear_solver_settings":{"rel_tol":1e-5,"abs_tol":1e-5,
+# predict dynamics with FE
+fe_setting = {"linear_solver_settings":{"solver":"JAX-direct"},
+                "nonlinear_solver_settings":{"rel_tol":1e-8,"abs_tol":1e-8,
                                             "maxiter":10,"load_incr":1}}
-nonlinear_fe_solver = FiniteElementNonLinearResidualBasedSolverHetero("nonlinear_fe_solver",thermal_loss_2d,fe_setting)
-nonlinear_fe_solver.Initialize()
-FE_T = np.zeros((fe_mesh.GetNumberOfNodes(),num_steps))
-FE_T_temp = coeffs_matrix[eval_id,:]
-for i in range(num_steps):
-    FE_T_temp = np.array(nonlinear_fe_solver.Solve(FE_T_temp,K_matrix,FE_T_temp))  #np.zeros(fe_mesh.GetNumberOfNodes())
-    FE_T[:,i] = FE_T_temp    
-absolute_error = np.abs(FOL_T- FE_T)
-fe_mesh['abs_error'] = absolute_error
-    
-fe_mesh['Heterogeneity'] = K_matrix.reshape(-1,1)
-fe_mesh['T_init'] = coeffs_matrix[eval_id,:].reshape(-1,1)
-fe_mesh['T_FOL'] = FOL_T
-fe_mesh['T_FE'] = FE_T
+nonlin_fe_solver = FiniteElementNonLinearResidualBasedSolver("nonlin_fe_solver",transient_thermal_loss_2d,fe_setting)
+nonlin_fe_solver.Initialize()
 
-# time_list = [int(num_steps/5) - 1,int(num_steps/2) - 1,num_steps - 1]
+T_fe_current = initial_solution.flatten()
+T_fes = jnp.array(T_fe_current)
+for _ in range(0,num_time_steps):
+    T_fe_next = np.array(nonlin_fe_solver.Solve(T_fe_current,np.zeros(fe_mesh.GetNumberOfNodes())))
+    T_fe_current = T_fe_next
+    T_fes = jnp.vstack((T_fes,T_fe_next.flatten()))
+
+# save the solutions
+for time_index, (T_ifol,T_fe) in enumerate(zip(T_ifols,T_fes)):
+    fe_mesh[f"T_ifol_{time_index}"] = np.array(T_ifol).reshape((fe_mesh.GetNumberOfNodes(), 1))
+    fe_mesh[f"T_fe_{time_index}"] = np.array(T_fe).reshape((fe_mesh.GetNumberOfNodes(), 1))
+
+fe_mesh["k0"] = np.array(train_heterogeneity_field.flatten()).reshape((fe_mesh.GetNumberOfNodes(), 1))
+
+
+absolute_error = np.abs(T_ifols- T_fes)
 time_list = [0,1,4,9,19,24,49]
 
-
-np.save(os.path.join(case_dir,"test2_FOL_T.npy"),FOL_T)
-np.save(os.path.join(case_dir,"test2_FE_T.npy"),FE_T)
-
-plot_mesh_vec_data_thermal_row(1,[coeffs_matrix[eval_id,:]],
+plot_mesh_vec_data_thermal_row(1,[train_temperature_fields[initial_solution_id,:]],
                    [""],
                    fig_title="",cmap = "jet",
-                   file_name=os.path.join(case_dir,"test2_initial_condition.png"))
+                   file_name=os.path.join(case_dir,"initial_condition.png"))
 
 plot_mesh_quad(fe_mesh.GetNodesCoordinates()[:,:-1],
                fe_mesh.GetElementsNodes("quad"),
-               background=hetero_info[::-1], 
-               filename=os.path.join(case_dir,"FE_mesh_hetero_info.png"))
+               background=train_heterogeneity_field[::-1], 
+               filename=os.path.join(case_dir,"FE_mesh_hetero_info.png"),show=False)
 
-plot_mesh_vec_data_thermal_row(1,[FOL_T[:,time_list[0]],FOL_T[:,time_list[1]],
-                                  FOL_T[:,time_list[2]],FOL_T[:,time_list[3]],
-                                  FOL_T[:,time_list[4]],FOL_T[:,time_list[5]],
-                                  FOL_T[:,time_list[6]]],
+
+plot_mesh_vec_data_thermal_row(1,[T_ifols[time_list[0],:],T_ifols[time_list[1],:],
+                                  T_ifols[time_list[2],:],T_ifols[time_list[3],:],
+                                  T_ifols[time_list[4],:],T_ifols[time_list[5],:],
+                                  T_ifols[time_list[6],:]],
                    ["","","","","","",""],
                    fig_title="",cmap = "jet",
                    file_name=os.path.join(case_dir,"test2_FOL_summary.png"))
-plot_mesh_vec_data_thermal_row(1,[FE_T[:,time_list[0]],FE_T[:,time_list[1]],
-                                  FE_T[:,time_list[2]],FE_T[:,time_list[3]],
-                                  FE_T[:,time_list[4]],FE_T[:,time_list[5]],
-                                  FE_T[:,time_list[6]]],
+plot_mesh_vec_data_thermal_row(1,[T_fes[time_list[0],:],T_fes[time_list[1],:],
+                                  T_fes[time_list[2],:],T_fes[time_list[3],:],
+                                  T_fes[time_list[4],:],T_fes[time_list[5],:],
+                                  T_fes[time_list[6],:]],
                    ["","","","","","",""],
                    fig_title="",cmap = "jet",
                    file_name=os.path.join(case_dir,"test2_FE_summary.png"))
 
-plot_mesh_vec_data_thermal_row(1,[absolute_error[:,time_list[0]],absolute_error[:,time_list[1]],
-                                  absolute_error[:,time_list[2]],absolute_error[:,time_list[3]],
-                                  absolute_error[:,time_list[4]],absolute_error[:,time_list[5]],
-                                  absolute_error[:,time_list[6]]],
+plot_mesh_vec_data_thermal_row(1,[absolute_error[time_list[0],:],absolute_error[time_list[1],:],
+                                  absolute_error[time_list[2],:],absolute_error[time_list[3],:],
+                                  absolute_error[time_list[4],:],absolute_error[time_list[5],:],
+                                  absolute_error[time_list[6],:]],
                    ["","","","","","",""],
                    fig_title="",cmap = "jet",
                    file_name=os.path.join(case_dir,"test2_Error_summary.png"))
+
 
 fe_mesh.Finalize(export_dir=case_dir)
--- fol/loss_functions/transient_thermal.py
+++ fol/loss_functions/transient_thermal.py
@@ -1,6 +1,7 @@
 """
- Authors: Reza Najian Asl, https://github.com/RezaNajian
- Date: April, 2025
+ Authors: Yusuke Yamazaki
+          Reza Najian Asl, https://github.com/RezaNajian
+ Date: May, 2025
  License: FOL/LICENSE
 """
 from  .thermal import ThermalLoss
@@ -38,31 +39,116 @@ class TransientThermalLoss(ThermalLoss):
         self.time_integration_settings = UpdateDefaultDict(self.default_time_integration_settings,self.loss_settings["time_integration_dict"])
         if self.time_integration_settings["time_step"] == None:
             fol_error("time step should be provided in the time_integration_dict ")
-
+    
     @partial(jit, static_argnums=(0,))
-    def ComputeElement(self,xyze,de,te,body_force=0):
+    def ComputeElement(self,xyze,Te_c,Te_n,Ke):
+        Te_c = Te_c.reshape(-1,1)
+        Te_n = Te_n.reshape(-1,1)
+        Ke = Ke.reshape(-1,1)
         @jit
-        def compute_at_gauss_point(gp_point,gp_weight,te):
-            te = jax.lax.stop_gradient(te)
-            N = self.fe_element.ShapeFunctionsValues(gp_point)
-            conductivity_at_gauss = jnp.dot(N, de.squeeze()) * (1 + 
-                                    self.material_settings["beta"]*(jnp.dot(N,te.squeeze()))**self.material_settings["c"])
-            DN_DX = self.fe_element.ShapeFunctionsGlobalGradients(xyze,gp_point)
+        def compute_at_gauss_point(gp_point,gp_weight):
+            N_vec = self.fe_element.ShapeFunctionsValues(gp_point)
+            T_at_gauss_n = jnp.dot(N_vec.reshape(1,-1), Te_n)
+            T_at_gauss_c = jnp.dot(N_vec.reshape(1,-1), Te_c)
+            K_at_gauss = jnp.dot(N_vec, Ke.squeeze()) * (1 + 
+                                    self.material_settings["beta"]*(T_at_gauss_n)**self.thermal_loss_settings["c"])
+            DN_DX = self.fe_element.ShapeFunctionsLocalGradients(gp_point)
             J = self.fe_element.Jacobian(xyze,gp_point)
             detJ = jnp.linalg.det(J)
-            gp_stiffness = conductivity_at_gauss * (DN_DX @ DN_DX.T) * detJ * gp_weight
-            gp_f = gp_weight * detJ * body_force *  N.reshape(-1,1) 
-            return gp_stiffness,gp_f
-        gp_points,gp_weights = self.fe_element.GetIntegrationData()
-        k_gps,f_gps = jax.vmap(compute_at_gauss_point,in_axes=(0,0,None))(gp_points,gp_weights,te)
-        Se = jnp.sum(k_gps, axis=0)
-        Fe = jnp.sum(f_gps, axis=0)
-        def compute_elem_res(Se,te ,Fe):
-            te = jax.lax.stop_gradient(te)
-            return (Se @ te - Fe)
-        element_residuals = compute_elem_res(Se,te ,Fe)
-        return  ((te.T @ element_residuals)[0,0]), (Se @ te - Fe), Se
+            invJ = jnp.linalg.inv(J)
+            B_mat = jnp.dot(invJ,DN_DX.T)
+            gp_stiffness = B_mat.T @ (K_at_gauss * B_mat) * detJ * gp_weight
+            gp_mass = self.material_settings["rho"] * self.material_settings["cp"]* jnp.outer(N_vec, N_vec) * detJ * gp_weight 
+            gp_t = self.material_settings["rho"] * self.material_settings["cp"] * 0.5/(self.time_integration_settings["time_step"])*gp_weight  * detJ *(T_at_gauss_n-T_at_gauss_c)**2
+            gp_dR = K_at_gauss * B_mat.T @ B_mat * detJ * gp_weight
+            return gp_stiffness,gp_mass, gp_t, gp_dR
 
+        gp_points,gp_weights = self.fe_element.GetIntegrationData()
+        k_gps,m_gps,t_gps, dR_gps = jax.vmap(compute_at_gauss_point,in_axes=(0,0))(gp_points,gp_weights)
+        Se = jnp.sum(k_gps, axis=0)
+        Me = jnp.sum(m_gps, axis=0)
+        Se_dR = jnp.sum(dR_gps, axis=0)
+        Te = jnp.sum(t_gps) 
+        element_residuals = jax.lax.stop_gradient((Me+self.time_integration_settings["time_step"]*Se)@Te_n - Me@Te_c)
+        element_weighted_residual_loss  = ((Te_n.T @ element_residuals)[0,0])
+        element_energy_loss = 0.5*Te_n.T@Se@Te_n + Te
+        return element_weighted_residual_loss, (Me+self.time_integration_settings["time_step"]*Se)@Te_n - Me@Te_c, (Me+self.time_integration_settings["time_step"]*Se_dR)
+
+    @partial(jit, static_argnums=(0,))
+    def ComputeElementEnergy(self,
+                             elem_xyz:jnp.array,
+                             elem_current_temps:jnp.array,
+                             elem_next_temps:jnp.array,
+                             elem_heterogeneity:jnp.array) -> float:
+        return self.ComputeElement(elem_xyz,elem_current_temps,elem_next_temps,elem_heterogeneity)[0]
+    
+    @partial(jit, static_argnums=(0,))
+    def ComputeElementEnergyVmapCompatible(self,
+                                           element_id:jnp.integer,
+                                           elements_nodes:jnp.array,
+                                           xyz:jnp.array,
+                                           nodal_current_temps:jnp.array,
+                                           nodal_next_temps:jnp.array,
+                                           nodal_heterogeneity:jnp.array):
+        return self.ComputeElementEnergy(xyz[elements_nodes[element_id],:],
+                                         nodal_current_temps[elements_nodes[element_id]],
+                                         nodal_next_temps[elements_nodes[element_id]],
+                                         nodal_heterogeneity[elements_nodes[element_id]])
+
+    @partial(jit, static_argnums=(0,))
+    def ComputeElementsEnergies(self,nodal_current_temps:jnp.array,nodal_next_temps:jnp.array):
+        # parallel calculation of energies
+        return jax.vmap(self.ComputeElementEnergyVmapCompatible,(0,None,None,None,None,None)) \
+                        (self.fe_mesh.GetElementsIds(self.element_type),
+                        self.fe_mesh.GetElementsNodes(self.element_type),
+                        self.fe_mesh.GetNodesCoordinates(),
+                        nodal_current_temps,
+                        nodal_next_temps,
+                        self.material_settings["k0"])
+    
+    @partial(jit, static_argnums=(0,))
+    def ComputeElementResidualAndJacobian(self,
+                                          elem_xyz:jnp.array,
+                                          elem_current_temps:jnp.array,
+                                          elem_next_temps:jnp.array,
+                                          elem_heterogeneity:jnp.array,
+                                          elem_BC:jnp.array,
+                                          elem_mask_BC:jnp.array,
+                                          transpose_jac:bool):
+        _,re,ke = self.ComputeElement(elem_xyz,elem_current_temps,elem_next_temps,elem_heterogeneity)
+
+       # Convert transpose_jac (bool) to an integer index (0 = False, 1 = True)
+        index = jnp.asarray(transpose_jac, dtype=jnp.int32)
+
+        # Define the two branches for switch
+        branches = [
+            lambda _: ke,                  # Case 0: No transpose
+            lambda _: jnp.transpose(ke)    # Case 1: Transpose ke
+        ]
+
+        # Apply the switch operation
+        ke = jax.lax.switch(index, branches, None)
+
+        return self.ApplyDirichletBCOnElementResidualAndJacobian(re,ke,elem_BC,elem_mask_BC)
+
+    @partial(jit, static_argnums=(0,))
+    def ComputeElementResidualAndJacobianVmapCompatible(self,element_id:jnp.integer,
+                                                        elements_nodes:jnp.array,
+                                                        xyz:jnp.array,
+                                                        nodal_current_temps:jnp.array,
+                                                        nodal_next_temps:jnp.array,
+                                                        full_dirichlet_BC_vec:jnp.array,
+                                                        full_mask_dirichlet_BC_vec:jnp.array,
+                                                        transpose_jac:bool):
+        return self.ComputeElementResidualAndJacobian(xyz[elements_nodes[element_id],:],
+                                                      nodal_current_temps[elements_nodes[element_id]],
+                                                      nodal_next_temps[elements_nodes[element_id]],
+                                                      self.material_settings["k0"][elements_nodes[element_id]],
+                                                      full_dirichlet_BC_vec[((self.number_dofs_per_node*elements_nodes[element_id])[:, jnp.newaxis] +
+                                                      jnp.arange(self.number_dofs_per_node))].reshape(-1,1),
+                                                      full_mask_dirichlet_BC_vec[((self.number_dofs_per_node*elements_nodes[element_id])[:, jnp.newaxis] +
+                                                      jnp.arange(self.number_dofs_per_node))].reshape(-1,1),
+                                                      transpose_jac)
 class TransientThermalLoss3DTetra(TransientThermalLoss):
     @print_with_timestamp_and_execution_time
     def __init__(self, name: str, loss_settings: dict, fe_mesh: Mesh):
