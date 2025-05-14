5e3cad23aa7ee267b37a92e4da453f9b01ca1b93
RezaNajian <r.najian@hotmail.com>
2025-05-14 06:12:55 +0200

    Merge branch 'transient-ifol'

---
+++ examples/paper_iFOL/transient_nonlinear_thermal_2D/meta_implicit_pl_transient_nonlinear_thermal_2D.py
@@ -0,0 +1,179 @@
+import sys
+import os
+import pickle,optax
+import numpy as np
+from fol.tools.usefull_functions import *
+from fol.tools.decoration_functions import *
+from fol.tools.logging_functions import Logger
+from fol.controls.identity_control import IdentityControl
+from fol.deep_neural_networks.meta_implicit_parametric_operator_learning import MetaImplicitParametricOperatorLearning
+from fol.solvers.fe_nonlinear_residual_based_solver import FiniteElementNonLinearResidualBasedSolver
+from fol.deep_neural_networks.nns import HyperNetwork,MLP
+from fol.loss_functions.transient_thermal import TransientThermalLoss2DQuad
+from thermal_usefull_functions import *
+import jax
+jax.config.update("jax_default_matmul_precision", "highest")
+
+# directory & save handling
+working_directory_name = 'meta_implicit_pl_transient_nonlinear_thermal_2D'
+case_dir = os.path.join('.', working_directory_name)
+create_clean_directory(working_directory_name)
+sys.stdout = Logger(os.path.join(case_dir,working_directory_name+".log"))
+
+# problem setup
+model_settings = {"L":1,"N":51,
+                "T_left":1.0,"T_right":0.0}
+
+# creation of the model
+fe_mesh = create_2D_square_mesh(L=model_settings["L"],N=model_settings["N"])
+fe_mesh.Initialize()
+
+# create some random fields for training
+create_random_fields = True
+if create_random_fields:
+    train_temperature_fields = generate_random_smooth_patterns(model_settings["L"],model_settings["N"],num_samples=9000)
+    train_heterogeneity_field = generate_morph_pattern(model_settings["N"]).reshape(1,-1) 
+    train_data_dict = {"temperatures":train_temperature_fields,"heterogeneity":train_heterogeneity_field}
+    with open(f'train_data_dict.pkl', 'wb') as f:
+        pickle.dump(train_data_dict,f)
+else:
+    with open(f'train_data_dict.pkl', 'rb') as f:
+        train_data_dict = pickle.load(f)
+    
+    train_temperature_fields = train_data_dict["temperatures"]
+    train_heterogeneity_field = train_data_dict["heterogeneity"]
+
+    fol_info(f"train temperature field {train_temperature_fields.shape} is imported !")
+    fol_info(f"train heterogeneity field {train_heterogeneity_field.shape} is imported !")
+    
+
+# create fe-based loss function
+bc_dict = {"T":{"left":model_settings["T_left"],"right":model_settings["T_right"]}}
+material_dict = {"rho":1.0,"cp":1.0,"k0":train_heterogeneity_field.flatten(),"beta":1.5,"c":1.0}
+time_integration_dict = {"method":"implicit-euler","time_step":0.005}
+transient_thermal_loss_2d = TransientThermalLoss2DQuad("thermal_transient_loss_2d",loss_settings={"dirichlet_bc_dict":bc_dict,
+                                                                            "num_gp":2,
+                                                                            "material_dict":material_dict,
+                                                                            "time_integration_dict":time_integration_dict},
+                                                                            fe_mesh=fe_mesh)
+transient_thermal_loss_2d.Initialize()
+
+identity_control = IdentityControl("ident_control",num_vars=fe_mesh.GetNumberOfNodes())
+identity_control.Initialize()
+
+# design synthesizer & modulator NN for hypernetwork
+characteristic_length = 256
+synthesizer_nn = MLP(name="synthesizer_nn",
+                     input_size=3,
+                     output_size=1,
+                     hidden_layers=[characteristic_length] * 6,
+                     activation_settings={"type":"sin",
+                                          "prediction_gain":30,
+                                          "initialization_gain":1.0},
+                     skip_connections_settings={"active":False,"frequency":1})
+
+latent_size = characteristic_length
+modulator_nn = MLP(name="modulator_nn",
+                   input_size=latent_size,
+                   use_bias=False) 
+
+hyper_network = HyperNetwork(name="hyper_nn",
+                             modulator_nn=modulator_nn,synthesizer_nn=synthesizer_nn,
+                             coupling_settings={"modulator_to_synthesizer_coupling_mode":"one_modulator_per_synthesizer_layer"})
+
+# create fol optax-based optimizer
+num_epochs = 10000
+learning_rate_scheduler = optax.linear_schedule(init_value=1e-4, end_value=1e-7, transition_steps=num_epochs)
+main_loop_transform = optax.chain(optax.normalize_by_update_norm(),optax.adam(learning_rate_scheduler))
+
+# create ifol
+ifol = MetaImplicitParametricOperatorLearning(name="meta_implicit_ol",control=identity_control,
+                                            loss_function=transient_thermal_loss_2d,
+                                            flax_neural_network=hyper_network,
+                                            main_loop_optax_optimizer=main_loop_transform,
+                                            latent_step_size=1e-2,
+                                            num_latent_iterations=3)
+ifol.Initialize()
+
+train_start_id = 0
+train_end_id = 6000
+
+ifol.Train(train_set=(train_temperature_fields[train_start_id:train_end_id,:],),
+            batch_size=120,
+            convergence_settings={"num_epochs":num_epochs,"relative_error":1e-100,"absolute_error":1e-100},
+            plot_settings={"plot_save_rate":100},
+            working_directory=case_dir)
+
+ifol.RestoreState(restore_state_directory=case_dir+"/flax_final_state")
+
+test_temperature_fields = generate_random_smooth_patterns_evaluation(model_settings["L"],model_settings["N"])
+
+# time ineference setup
+initial_solution_id = 0 
+initial_solution = test_temperature_fields[initial_solution_id]
+num_time_steps = 50
+
+# predict dynamics with ifol
+T_ifols = ifol.PredictDynamics(initial_solution,num_time_steps)
+
+# predict dynamics with FE
+fe_setting = {"linear_solver_settings":{"solver":"JAX-bicgstab","tol":1e-6,"atol":1e-6,
+                                                "maxiter":1000,"pre-conditioner":"ilu"},
+                "nonlinear_solver_settings":{"rel_tol":1e-8,"abs_tol":1e-8,
+                                            "maxiter":10,"load_incr":1}}
+nonlin_fe_solver = FiniteElementNonLinearResidualBasedSolver("nonlin_fe_solver",transient_thermal_loss_2d,fe_setting)
+nonlin_fe_solver.Initialize()
+
+T_fe_current = initial_solution.flatten()
+T_fes = jnp.array(T_fe_current)
+for _ in range(0,num_time_steps):
+    T_fe_next = np.array(nonlin_fe_solver.Solve(T_fe_current,T_fe_current))
+    T_fe_current = T_fe_next
+    T_fes = jnp.vstack((T_fes,T_fe_next.flatten()))
+
+# save the solutions
+for time_index, (T_ifol,T_fe) in enumerate(zip(T_ifols,T_fes)):
+    fe_mesh[f"T_ifol_{time_index}"] = np.array(T_ifol).reshape((fe_mesh.GetNumberOfNodes(), 1))
+    fe_mesh[f"T_fe_{time_index}"] = np.array(T_fe).reshape((fe_mesh.GetNumberOfNodes(), 1))
+
+fe_mesh["k0"] = np.array(train_heterogeneity_field.flatten()).reshape((fe_mesh.GetNumberOfNodes(), 1))
+
+
+absolute_error = np.abs(T_ifols- T_fes)
+time_list = [0,1,4,9,19,24,49]
+
+plot_mesh_vec_data_thermal_row(1,[test_temperature_fields[initial_solution_id,:]],
+                   [""],
+                   fig_title="",cmap = "jet",
+                   file_name=os.path.join(case_dir,"initial_condition.png"))
+
+plot_mesh_quad(fe_mesh.GetNodesCoordinates()[:,:-1],
+               fe_mesh.GetElementsNodes("quad"),
+               background=train_heterogeneity_field.reshape((model_settings["N"],model_settings["N"]))[::-1], 
+               filename=os.path.join(case_dir,"FE_mesh_hetero_info.png"),show=False)
+
+plot_mesh_vec_data_thermal_row(1,[T_ifols[time_list[0],:],T_ifols[time_list[1],:],
+                                  T_ifols[time_list[2],:],T_ifols[time_list[3],:],
+                                  T_ifols[time_list[4],:],T_ifols[time_list[5],:],
+                                  T_ifols[time_list[6],:]],
+                   ["","","","","","",""],
+                   fig_title="",cmap = "jet",
+                   file_name=os.path.join(case_dir,"test2_FOL_summary.png"))
+plot_mesh_vec_data_thermal_row(1,[T_fes[time_list[0],:],T_fes[time_list[1],:],
+                                  T_fes[time_list[2],:],T_fes[time_list[3],:],
+                                  T_fes[time_list[4],:],T_fes[time_list[5],:],
+                                  T_fes[time_list[6],:]],
+                   ["","","","","","",""],
+                   fig_title="",cmap = "jet",
+                   file_name=os.path.join(case_dir,"test2_FE_summary.png"))
+
+plot_mesh_vec_data_thermal_row(1,[absolute_error[time_list[0],:],absolute_error[time_list[1],:],
+                                  absolute_error[time_list[2],:],absolute_error[time_list[3],:],
+                                  absolute_error[time_list[4],:],absolute_error[time_list[5],:],
+                                  absolute_error[time_list[6],:]],
+                   ["","","","","","",""],
+                   fig_title="",cmap = "jet",
+                   file_name=os.path.join(case_dir,"test2_Error_summary.png"))
+
+
+fe_mesh.Finalize(export_dir=case_dir)
---
+++ tests/unit/test_nonlinear_transient_thermal.py
@@ -0,0 +1,144 @@
+import pytest
+import unittest 
+import os
+import numpy as np
+import os
+os.environ["JAX_PLATFORM_NAME"] = "cpu"
+from fol.loss_functions.transient_thermal import TransientThermalLoss2DQuad
+from fol.loss_functions.transient_thermal import TransientThermalLoss2DTri
+from fol.loss_functions.transient_thermal import TransientThermalLoss3DHexa
+from fol.loss_functions.transient_thermal import TransientThermalLoss3DTetra
+from fol.tools.usefull_functions import *
+
+
+class TestNonlinearTransientThermal(unittest.TestCase):
+
+    @pytest.fixture(autouse=True)
+    def _request_debug_mode(self,request):
+        self.debug_mode = request.config.getoption('--debug-mode')
+
+    def setUp(self):
+        # problem setup
+        self.fe_mesh = Mesh("box_io","box_3D_coarse.med",os.path.join(os.path.dirname(os.path.abspath(__file__)),"../meshes"))
+        self.dirichlet_bc_dict = {"T":{"left":1.0,"right":0.0}}
+        self.material_dict = {"rho":1.0,"cp":1.0,"k0":np.ones(1213),"beta":1.5,"c":1.0}
+        self.time_integration_dict = {"method":"implicit-euler","time_step":0.005}
+
+        self.fe_mesh.Initialize()
+
+    def test_tetra(self):
+        thermal_loss_3d = TransientThermalLoss3DTetra("transient_thermal_loss_3d",loss_settings={"dirichlet_bc_dict":self.dirichlet_bc_dict,
+                                                                            "material_dict":self.material_dict,
+                                                                            "time_integration_dict":self.time_integration_dict},
+                                                                            fe_mesh=self.fe_mesh)
+        thermal_loss_3d.Initialize()
+        
+        tet_points_coordinates = jnp.array([[0.1, 0.1, 0.1],
+                                            [0.28739360416666665, 0.27808503701741405, 0.05672979583333333],
+                                            [0.0, 1.0, 0.0],
+                                            [0.0, 1.0, 0.1]])
+        en, residuals, stiffness = thermal_loss_3d.ComputeElement(tet_points_coordinates,
+                                                                        jnp.ones((4)),
+                                                                        jnp.zeros((4,1)),
+                                                                        jnp.ones((4,1)))
+
+        np.testing.assert_allclose(stiffness,jnp.array([[5.377708002924919128e-03,-8.257009321823716164e-04,-1.326714846072718501e-04,-3.642407711595296860e-03],
+                                                        [-8.257009321823716164e-04,6.439753924496471882e-04,2.637726720422506332e-04,6.948809023015201092e-04],
+                                                        [-1.326714846072718501e-04,2.637726720422506332e-04,2.233165432699024677e-04,4.225104348734021187e-04],
+                                                        [-3.642407711595296860e-03,6.948809023015201092e-04,4.225104348734021187e-04,3.301944816485047340e-03]])
+                                                        ,rtol=1e-5, atol=1e-10)
+
+        np.testing.assert_allclose(residuals.flatten(),jnp.array([-7.769281510263681412e-04,-7.769281510263681412e-04,-7.769281510263681412e-04,-7.769281510263681412e-04])
+                                   ,rtol=1e-5, atol=1e-10)
+
+    def test_hexa(self):
+        hex_points_coordinates = jnp.array([[0.24900,  0.34200,  0.19200],
+                                            [0.32000,  0.18600,  0.64300],
+                                            [0.16500,  0.74500,  0.70200],
+                                            [0.27300,  0.75000,  0.23000],
+                                            [0.00000,  0.00000,  0.00000],
+                                            [0.00000,  0.00000,  1.00000],
+                                            [0.00000,  1.00000,  1.00000],
+                                            [0.00000,  1.00000,  0.00000]])
+        
+        transient_thermal_3d = TransientThermalLoss3DHexa("transient_thermal_loss_3d",loss_settings={"dirichlet_bc_dict":self.dirichlet_bc_dict,
+                                                                            "material_dict":self.material_dict,
+                                                                            "time_integration_dict":self.time_integration_dict},
+                                                                            fe_mesh=self.fe_mesh)
+        transient_thermal_3d.Initialize()
+
+        en, residuals, stiffness = transient_thermal_3d.ComputeElement(hex_points_coordinates,
+                                                                        jnp.ones((8)),
+                                                                        jnp.zeros((8,1)),
+                                                                        jnp.ones((8,1)))
+
+        np.testing.assert_allclose(stiffness,jnp.array([[4.569048993289470673e-03,1.371352816931903362e-03,2.573407255113124847e-04,2.214400796219706535e-03,3.225729567930102348e-03,7.280206191353499889e-04,1.327938662143424153e-04,1.554277492687106133e-03],
+                                                        [1.371352816931903362e-03,5.164207424968481064e-03,1.970592420548200607e-03,4.943234962411224842e-04,8.051422191783785820e-04,3.525064326822757721e-03,1.449034665711224079e-03,3.043211472686380148e-04],
+                                                        [2.573407255113124847e-04,1.970592420548200607e-03,4.087680950760841370e-03,5.598774878308176994e-04,2.434534981148317456e-04,1.510155620053410530e-03,3.062376752495765686e-03,5.048538441769778728e-04],
+                                                        [2.214400796219706535e-03,4.943234962411224842e-04,5.598774878308176994e-04,4.713998176157474518e-03,1.580047886818647385e-03,2.371072041569277644e-04,2.262281632283702493e-04,3.236241638660430908e-03],
+                                                        [3.225729567930102348e-03,8.051422191783785820e-04,2.434534981148317456e-04,1.580047886818647385e-03,9.222630411386489868e-03,2.222476759925484657e-03,7.334632100537419319e-04,4.500711802393198013e-03],
+                                                        [7.280206191353499889e-04,3.525064326822757721e-03,1.510155620053410530e-03,2.371072041569277644e-04,2.222476759925484657e-03,9.997963905334472656e-03,4.345113877207040787e-03,8.268423844128847122e-04],
+                                                        [1.327938662143424153e-04,1.449034665711224079e-03,3.062376752495765686e-03,2.262281632283702493e-04,7.334632100537419319e-04,4.345113877207040787e-03,8.648800663650035858e-03,1.327868434600532055e-03],
+                                                        [1.554277492687106133e-03,3.043211472686380148e-04,5.048538441769778728e-04,3.236241638660430908e-03,4.500711802393198013e-03,8.268423844128847122e-04,1.327868434600532055e-03,9.167869575321674347e-03]]),
+                                   rtol=1e-5, atol=1e-10)
+        
+        np.testing.assert_allclose(residuals.flatten(),jnp.array([-1.405296474695205688e-02,-1.508403755724430084e-02,-1.219633128494024277e-02,-1.326222531497478485e-02,-2.253365516662597656e-02,-2.339274436235427856e-02,-1.992568001151084900e-02,-2.142298594117164612e-02]),
+                                   rtol=1e-5, atol=1e-10)
+        
+    def test_tri(self):
+                
+        quad_points_coordinates = jnp.array([[1.00,0.00,0.00],
+                                            [0.50,0.50,0.00],
+                                            [0.00,0.00,0.00]])
+
+        transient_thermal_3d = TransientThermalLoss2DTri("transient_thermal_2d",
+                                                  loss_settings={"dirichlet_bc_dict":self.dirichlet_bc_dict,
+                                                                            "material_dict":self.material_dict,
+                                                                            "time_integration_dict":self.time_integration_dict},
+                                                                 fe_mesh=self.fe_mesh)
+        transient_thermal_3d.Initialize()
+
+        en, residuals, stiffness = transient_thermal_3d.ComputeElement(quad_points_coordinates,
+                                                                     jnp.ones((3)),
+                                                                     jnp.zeros((3,1)),
+                                                                     jnp.ones((3,1)))
+
+        np.testing.assert_allclose(stiffness,jnp.array([[3.777776658535003662e-02,2.527777478098869324e-02,2.027777396142482758e-02],
+                                                        [2.527777478098869324e-02,2.902778051793575287e-02,2.902778051793575287e-02],
+                                                        [2.027777396142482758e-02,2.902778051793575287e-02,3.402777761220932007e-02]]),
+                                   rtol=1e-5, atol=1e-10)
+        
+        np.testing.assert_allclose(residuals.flatten(),jnp.array([-8.333332091569900513e-02,-8.333332836627960205e-02,-8.333332836627960205e-02]),
+                                   rtol=1e-5, atol=1e-10)
+        
+    def test_quad(self):
+                
+        quad_points_coordinates = jnp.array([[1.00,0.00,0.00],
+                                            [1.00,1.00,0.00],
+                                            [0.00,1.00,0.00],
+                                            [0.00,0.00,0.00]])
+
+        transient_thermal_2d = TransientThermalLoss2DQuad("transient_thermal_2d",
+                                                  loss_settings={"dirichlet_bc_dict":self.dirichlet_bc_dict,
+                                                                            "material_dict":self.material_dict,
+                                                                            "time_integration_dict":self.time_integration_dict},
+                                                                 fe_mesh=self.fe_mesh)
+        transient_thermal_2d.Initialize()
+                
+        en, residuals, stiffness = transient_thermal_2d.ComputeElement(quad_points_coordinates,
+                                                                     jnp.ones((4)),
+                                                                     jnp.zeros((4,1)),
+                                                                     jnp.ones((4,1)))
+
+        np.testing.assert_allclose(stiffness,jnp.array([[1.144444271922111511e-01,5.472222343087196350e-02,2.611111290752887726e-02,5.472222343087196350e-02],
+                                                        [5.472222343087196350e-02,1.144444271922111511e-01,5.472222343087196350e-02,2.611111290752887726e-02],
+                                                        [2.611111290752887726e-02,5.472222343087196350e-02,1.144444271922111511e-01,5.472222343087196350e-02],
+                                                        [5.472222343087196350e-02,2.611111290752887726e-02,5.472222343087196350e-02,1.144444271922111511e-01]])
+                                                        ,rtol=1e-5, atol=1e-10)
+
+        np.testing.assert_allclose(residuals.flatten(),jnp.array([-2.499999850988388062e-01,-2.499999850988388062e-01,-2.499999850988388062e-01,-2.500000000000000000e-01])
+                                   ,rtol=1e-5, atol=1e-10)
+
+
+if __name__ == '__main__':
+    unittest.main()
