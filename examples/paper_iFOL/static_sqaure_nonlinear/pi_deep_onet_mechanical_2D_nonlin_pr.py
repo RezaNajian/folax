f7487d42ed68ded58225aa82f249d9c0d35027d3
RezaNajian <62375973+RezaNajian@users.noreply.github.com>
2025-06-27 17:03:47 +0200

    Update pi_deep_onet_mechanical_2D_nonlin_pr.py

--- examples/paper_iFOL/static_sqaure_nonlinear/pi_deep_onet_mechanical_2D_nonlin_pr.py
+++ examples/paper_iFOL/static_sqaure_nonlinear/pi_deep_onet_mechanical_2D_nonlin_pr.py
@@ -16,7 +16,7 @@ import pickle
 import jax
 
 jax.config.update('jax_default_matmul_precision','high')
-jax.config.update('jax_enable_x64', True)
+# jax.config.update('jax_enable_x64', True)
 
 # directory & save handling
 working_directory_name = 'pi_deep_onet_mechanical_2D'
@@ -42,30 +42,22 @@ fourier_control_settings = {"x_freqs":np.array([2,4,6]),"y_freqs":np.array([2,4,
 fourier_control = FourierControl("fourier_control",fourier_control_settings,fe_mesh)
 fourier_control.Initialize()
 
-# create some random coefficients & K for training
-create_random_coefficients = False
-if create_random_coefficients:
-    number_of_random_samples = 10
-    coeffs_matrix,K_matrix = create_random_fourier_samples(fourier_control,number_of_random_samples)
-    export_dict = model_settings.copy()
-    export_dict["coeffs_matrix"] = coeffs_matrix
-    export_dict["x_freqs"] = fourier_control.x_freqs
-    export_dict["y_freqs"] = fourier_control.y_freqs
-    export_dict["z_freqs"] = fourier_control.z_freqs
-    with open(f'fourier_control_dict.pkl', 'wb') as f:
-        pickle.dump(export_dict,f)
-else:
-    with open(f'fourier_control_dict.pkl', 'rb') as f:
-        loaded_dict = pickle.load(f)
-    
-    coeffs_matrix = loaded_dict["coeffs_matrix"]
+# with open(f'train_data_dict.pkl', 'rb') as f:
+#     train_data_dict = pickle.load(f)
+
+# coeffs_matrix = train_data_dict["input_matrix"]
+
+with open(f'fourier_control_dict.pkl', 'rb') as f:
+    loaded_dict = pickle.load(f)
+
+coeffs_matrix = loaded_dict["coeffs_matrix"]
 
 K_matrix = fourier_control.ComputeBatchControlledVariables(coeffs_matrix)
 
 material_dict = {"young_modulus":1,"poisson_ratio":0.3}
 loss_settings={"dirichlet_bc_dict":bc_dict,
                "material_dict":material_dict,
-               "loss_function_exponent":2.0}
+               "loss_function_exponent":1.0}
 mechanical_loss_2d = NeoHookeMechanicalLoss2DQuad("mechanical_loss_2d",loss_settings=loss_settings,
                                                                             fe_mesh=fe_mesh)
 mechanical_loss_2d.Initialize()
@@ -90,12 +82,13 @@ deep_onet = DeepONet("main_deeponet",
                      branch_nn=branch_nn,
                      trunk_nn=trunk_nn,
                      output_dimension=num_dofs,
-                     activation_function_name=activation_function,
-                     use_bias=True,
+                     activation_function_name=None,
+                     use_bias=False,
                      output_scale_factor=0.001)
 
-num_epochs = 5000
-optimizer = optax.chain(optax.adam(1e-5))
+num_epochs = 100000
+learning_rate_scheduler = optax.linear_schedule(init_value=1e-4, end_value=1e-6, transition_steps=num_epochs)
+optimizer = optax.chain(optax.adam(1e-6))
 
 # create fol
 deeponet_learning = PhysicsInformedDeepONetParametricOperatorLearning(name="deeponet_learning",
@@ -105,26 +98,31 @@ deeponet_learning = PhysicsInformedDeepONetParametricOperatorLearning(name="deep
                                                                         optax_optimizer=optimizer)
 
 deeponet_learning.Initialize()
+
 train_start_id = 0
-train_end_id = 10
-train_set_pr = coeffs_matrix[train_start_id:train_end_id,:]     # for parametric training
+train_end_id = 8000
+test_start_id = 8000
+test_end_id = 10000
+
 #here we train for single sample at eval_id but one can easily pass the whole coeffs_matrix
-deeponet_learning.Train(train_set=(train_set_pr,),
-                        batch_size=1,
+deeponet_learning.Train(train_set=(coeffs_matrix[train_start_id:train_end_id,:],),
+                        test_set=(coeffs_matrix[test_start_id:test_end_id,:],),
+                        test_frequency=100,
+                        batch_size=350,
                         convergence_settings={"num_epochs":num_epochs,"relative_error":1e-100,"absolute_error":1e-100},
                         plot_settings={"plot_save_rate":100},
-                        train_checkpoint_settings={"least_loss_checkpointing":False,"frequency":10},
+                        train_checkpoint_settings={"least_loss_checkpointing":True,"frequency":100},
                         working_directory=case_dir)
 
 # load teh best model
 deeponet_learning.RestoreState(restore_state_directory=case_dir+"/flax_final_state")
 
-fe_setting = {"linear_solver_settings":{"solver":"JAX-direct","tol":1e-6,"atol":1e-6,
-                                                "maxiter":1000,"pre-conditioner":"ilu"},
+fe_setting = {"linear_solver_settings":{"solver":"JAX-direct","tol":1e-10,"atol":1e-10,
+                                                "maxiter":10000,"pre-conditioner":"ilu"},
                     "nonlinear_solver_settings":{"rel_tol":1e-8,"abs_tol":1e-8,
                                                 "maxiter":20,"load_incr":40}}
 
-for test in np.arange(train_start_id,train_end_id):
+for test in [1000,2000,3000,4000,5000,6000,7000,8000,8500,9000,9500,9999]:
     eval_id = test
     FOL_UV = np.array(deeponet_learning.Predict(coeffs_matrix[eval_id,:].reshape(-1,1).T)).reshape(-1)
     fe_mesh[f'U_FOL_{eval_id}'] = FOL_UV.reshape((fe_mesh.GetNumberOfNodes(), 2))
@@ -133,20 +131,19 @@ for test in np.arange(train_start_id,train_end_id):
     ## solve FE here
     linear_fe_solver = FiniteElementNonLinearResidualBasedSolver("linear_fe_solver",mechanical_loss_2d,fe_setting)
     linear_fe_solver.Initialize()
-    FE_UV = np.array(linear_fe_solver.Solve(K_matrix[eval_id],jnp.zeros(2*fe_mesh.GetNumberOfNodes())))
+    FE_UV = np.array(linear_fe_solver.Solve(K_matrix[eval_id].flatten(),jnp.zeros(2*fe_mesh.GetNumberOfNodes())))
     fe_mesh[f'U_FE_{eval_id}'] = FE_UV.reshape((fe_mesh.GetNumberOfNodes(), 2))
 
     absolute_error = abs(FOL_UV.reshape(-1,1)- FE_UV.reshape(-1,1))
     fe_mesh['abs_error'] = absolute_error.reshape((fe_mesh.GetNumberOfNodes(), 2))
 
     # plot U
-    vectors_list = [K_matrix[eval_id,:],FOL_UV[::2],FE_UV[::2]]
+    vectors_list = [K_matrix[eval_id].flatten(),FOL_UV[::2],FE_UV[::2]]
     plot_mesh_res(vectors_list, file_name=case_dir+f'/plot_U_{eval_id}.png',dir="U")
 
     # plot V
-    vectors_list = [K_matrix[eval_id,:],FOL_UV[1::2],FE_UV[1::2]]
+    vectors_list = [K_matrix[eval_id].flatten(),FOL_UV[1::2],FE_UV[1::2]]
     plot_mesh_res(vectors_list, file_name=case_dir+f'/plot_V_{eval_id}.png',dir="V")
-
 
 fe_mesh.Finalize(export_dir=case_dir, export_format='vtk')
 
