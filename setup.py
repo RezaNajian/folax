626c2231d49539056b65babe137b620b70b2e0e1
Jerry Paul Varghese <141227487+jerrypaulvarghese@users.noreply.github.com>
2025-08-28 23:38:11 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fol',
+    version='0.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'meshio',
+        'jax',
+        'flax',
+        'optax',
+        'gmsh',
+        'matplotlib',
+        'tqdm',
+        'numpy',
+        'scipy',
+        'h5py',
+        'pytest',
+        'zarr',
+        'dask',
+        'pyvista'
+    ],
+    author='FOL team',
+    author_email='r.najian@hotmail.com',
+    description='Bridging Neural Operators and Numerical Methods',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/RezaNajian/FOL',  
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires='>=3.10',
+)
